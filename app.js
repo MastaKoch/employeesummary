da9b117f1de01138d5231c2633e43660cc6a3a07
MastaKoch <alexkoch1996@gmail.com>
2021-01-13 11:03:31 -0500

    starting the process of writing the array of questions

--- app.js
+++ app.js
@@ -11,8 +11,17 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
+// render(employees)
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+const employee = [
+    {
+        name: "What is your name?",
+        id: "What is your employee ID?",
+    }
+];
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
