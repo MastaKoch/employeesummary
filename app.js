08ab19267f9fa9103d10fade830ac2d690d24e9d
MastaKoch <alexkoch1996@gmail.com>
2021-01-15 10:10:03 -0500

    almost done. getting an undefined error for office number

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ const { kMaxLength } = require("buffer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! 
 const employeesArr = [];
 
 const employeeQuest = [
@@ -144,30 +164,43 @@ function assembleTeam() {
 
                         console.log("A new manager has been added to your team!");
                         assembleTeam();  
-                })
-            } 
-               
-        })
-    
-    
-}
+                });
+            } else if (response.position === "Engineer") {
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+                inquirer.prompt(engineerQuestions).then(response => {
+                    var newEngineer= new Engineer(
+                        response.name,
+                        response.id,
+                        response.email,
+                        response.github,
+                    );
+                    employeesArr.push(newEngineer);
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+                    console.log("A new engineer has been added to your team!");
+                    assembleTeam();
+                });
+            } else if (response.position === "Intern") {
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+                inquirer.prompt(internQuestions).then(response => {
+                    var newIntern= new Intern(
+                        response.name,
+                        response.id,
+                        response.email,
+                        response.school,
+                    );
+                    employeesArr.push(newIntern);
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+                    console.log("A new intern has been added to your team!");
+                    assembleTeam();
+                
+        });
+            
+        } else {
+            var main = render(employeesArr);
+            fs.writeFile(outputPath, main, (err) => {
+                if (err) throw err;
+                console.log("Your new team has been generated in the team.html file!");
+            })
+        };
+
+    })}
